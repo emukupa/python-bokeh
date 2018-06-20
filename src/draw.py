ef4988f087e4407af706436838823732a5283313
Edward Manda <lilkups@yahoo.com>
2018-06-20 16:45:38 -0500

    Figured out the start and end

--- src/draw.py
+++ src/draw.py
@@ -9,37 +9,47 @@ from graph import *
 
 graph_data = Graph()
 graph_data.debug_create_test_data()
-#print(graph_data.vertexes)
+# print(graph_data.vertexes)
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
+# print(node_indices)
 
-print('hello')
+start = []
+end = []
+
 color_list = []
-for vertex in graph_data.vertexes:
+for i, vertex in enumerate(graph_data.vertexes):
     color_list.append(vertex.color)
+    if(len(vertex.edges)):
+        for edge in vertex.edges:
+            j = graph_data.vertexes.index(edge.destination)
+            start.append(i)
+            end.append(j)
 
-#plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500), tools='', toolbar_location=None)
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1, 1.1), y_range=(-1.1, 1.1),
-              tools='', toolbar_location=None)
+print(start, end)
+
+plot = figure(title='Graph Layout Demonstration', x_range=(
+    0, 500), y_range=(0, 500), tools='', toolbar_location=None)
+#plot = figure(title='Graph Layout Demonstration', x_range=(-1.1, 1.1), y_range=(-1.1, 1.1), tools='', toolbar_location=None)
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.glyph = Oval(height=25, width=25, fill_color='color')
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
-
+#graph.edge_renderer.data_source.data = dict(start=[0]*N, end=node_indices)
+graph.edge_renderer.data_source.data = dict(start=start, end=end)
 # start of layout code
 
+'''
 circ = [i*2*math.pi/8 for i in node_indices]
 x = [math.cos(i) for i in circ]
 y = [math.sin(i) for i in circ]
+'''
 
-#x = [v.pos['x'] for v in graph_data.vertexes]
-#y = [v.pos['y'] for v in graph_data.vertexes]
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
--- src/graph.py
+++ src/graph.py
@@ -19,11 +19,20 @@ class Graph:
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=60, y=140)
         debug_vertex_3 = Vertex('t3', x=400, y=340)
+        debug_vertex_4 = Vertex('t4', x=200, y=90)
+        debug_vertex_5 = Vertex('t5', x=100, y=240)
+        debug_vertex_6 = Vertex('t6', x=300, y=40)
 
-        debug_edge_1 = Edge(debug_vertex_2)  # 1 -> 2
+        debug_edge_1 = Edge(debug_vertex_2)  # 1 -> 2,  index 0 -> index 1
         debug_vertex_1.edges.append(debug_edge_1)
 
         debug_edge_2 = Edge(debug_vertex_2)
+        # 3 -> 2, index 2 ->  index 1
         debug_vertex_3.edges.append(debug_edge_2)
 
-        self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3])
+        debug_edge_5 = Edge(debug_vertex_5)  # 4 -> 5 index 3 -> index 4
+        debug_vertex_4.edges.append(debug_edge_5)
+
+        # start=[0, 2, 4], end=[1,1, 5]
+        self.vertexes.extend(
+            [debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4, debug_vertex_5, debug_vertex_6])
